695b50be47a3712e2214bfc9b173697ade4ef8c6
ScamCast <56202326+ScamCast@users.noreply.github.com>
2020-01-15 22:00:50 -0700

    Dlive Scraper update

--- misc/dlive_scraper.py
+++ misc/dlive_scraper.py
@@ -1,100 +1,202 @@
-import requests
 from time import sleep
-from pprint import pprint as pp
-END_POINT = "https://graphigo.prd.dlive.tv/"
-dlive_users = {}
+import requests
+import traceback
+import re
 
-def query_user(user):
-    output = '''query{
-    userByDisplayName(displayname:"''' + user + '''") {
-        id
-        username
-        displayname
-        avatar
-        partnerStatus
-        livestream {
-            permlink
-            title
-            thumbnailUrl
-            createdAt
-            watchingCount
-            view
-        }
-        hostingLivestream {
-            creator {
+
+
+END_POINT = "https://graphigo.prd.dlive.tv/"
+
+
+
+def match_valid_user(text):
+
+    if re.match('^[0-9A-Za-z_]+$', text):
+        return text
+
+    is_valid_url = re.search('https?\:\/\/.*\/([0-9A-Za-z_]+).*', text)
+    if is_valid_url:
+        return is_valid_url.group(1)
+
+    remove_invalid = ""
+    for char in text:
+        if not re.match('[0-9A-Za-z_]', char):
+            return False
+
+    return False
+
+
+
+def batch_queries(users, batch_size=8):
+    output = ""
+    queries = []
+    ticker = 0
+    num_users = len(users)
+
+    for user in users:
+
+        ticker += 1
+        num_users -= 1
+        output += f'{user}: userByDisplayName(displayname:"{user}")' + '''
+        {
+            id
+            username
+            displayname
+            avatar
+            offlineImage
+            partnerStatus
+            banStatus
+            deactivated
+            livestream {
+                permlink
+                title
+                thumbnailUrl
+                createdAt
+                watchingCount
+                view
                 id
             }
+            hostingLivestream {
+                creator {
+                    id
+                }
+            }
+            followers {
+                totalCount
+            }
         }
-        followers {
-            totalCount
-        }
-    }
-}'''
-    return {'query': output}
+        '''
+        if ticker == batch_size or num_users == 0:
+            queries.append({'query': 'query{' + output + '}'})
+            output = ""
+            ticker = 0
+
+    return queries
+
 
 
 def parse_user(data):
 
+    output = {}
 
-
-    ######## NOTES: Values are returning NONE, need to set values before 'try'
     try:
-        user = data['data']['userByDisplayName']
-        username = user.get('displayname')
-        output = {
-                username: {
-                        'ch_platform': 'DLIVE',
-                        'ch_info_id': username,
-                        'ch_info_name': username,
-                        'ch_media_avatar': user.get('avatar'),
-                        # 'ch_media_offline': None,
-                        # 'ch_info_deactivated': False,
-                        # 'ch_info_banned': False,
-                        'ch_info_partner': user.get('partnerStatus'),
-                        # 'ch_info_num_broadcasts': 0,
-                        # 'ch_info_num_videos': 0,
-                        # 'ch_info_following': 0,
-                        'ch_info_followers': user.get('followers', {}).get('totalCount', 0),
-                        'ch_live': True if user.get('livestream') else False,
-                        'ch_live_title': user.get('livestream', {}).get('title'),
-                        'ch_live_viewers': user.get('livestream', {}).get('watchingCount', 0),
-                        'ch_live_hosting': True if user.get('hostingLivestream') else False,
+        for chan in data['data']:
+
+            data_user = data['data'][chan]
+
+            if not data_user:
+                continue
+
+            data_live = data_user.get('livestream') if data_user.get('livestream') else {}
+
+            platform = 'DLIVE'
+            username = data_user.get('displayname')
+            image_avatar = data_user.get('avatar')
+            image_offline = data_user.get('offlineImage')
+            partner = data_user.get('partnerStatus')
+            num_followers = data_user.get('followers', {}).get('totalCount', 0)
+            # is_banned: NO_BAN, BAN_FROM_STREAMING, ACCOUNT_SUSPENDED
+            is_banned = False if data_user.get('banStatus') == "NO_BAN" else  data_user.get('banStatus')
+            is_deactivated = data_user.get('deactivated')
+            is_hosting = True if data_user.get('hostingLivestream') else False
+            live = True if data_live else False
+            live_id = data_live.get('id')
+            live_title = data_live.get('title')
+            live_viewers = data_live.get('watchingCount', 0)
+
+            chan_formatted_data = {
+                    username: {
+                            'ch_platform': platform,
+                            'ch_info_id': username,
+                            'ch_info_name': username,
+                            'ch_media_avatar': image_avatar,
+                            'ch_media_offline': image_offline,
+                            'ch_info_deactivated': is_deactivated,
+                            'ch_info_banned': is_banned,
+                            'ch_info_partner': partner,
+                            'ch_info_num_broadcasts': 0,
+                            'ch_info_num_videos': 0,
+                            'ch_info_following': 0,
+                            'ch_info_followers': num_followers,
+                            'ch_live': live,
+                            'ch_live_id': live_id,
+                            'ch_live_title': live_title,
+                            'ch_live_viewers': live_viewers,
+                            'ch_live_hosting': is_hosting,
+                        }
                     }
-                }
+            output.update(chan_formatted_data)
+
     except Exception as e:
-        print(e)
+        print(traceback.format_exc())
         return False
+
     return output
 
 
-def dlive_request(users):
-    global dlive_users
+
+def dlive_request(users, batch_size=8):
+    """
+        dlive_request(users=['user1', 'user2', 'user3'])
+
+    :users: list|str
+        Example:
+            users = ['user1', 'user2', 'user3']
+            or
+            users = 'user1'
+
+    :returns dict
+        Example:
+            {
+                'user1': {...},
+                'user2': {...}
+            }
+    """
+
     if isinstance(users, str):
         users = [users]
 
-    session = requests.session()
-    retries = 3
+    dlive_users = {}
 
-    for user in users:
+    # Only query valid usernames
+    for u in users:
+        valid_user = match_valid_user(u)
+        if valid_user:
+            dlive_users[valid_user] = {}
+
+    query_batches = batch_queries(dlive_users.keys(), batch_size=batch_size)
+
+    session = requests.session()
+
+    for batch in query_batches:
+
+        retries = 5
         dlive_user = {}
+
         while True:
             try:
                 retries -= 1
-                response = session.post(END_POINT, json=query_user(user)).json()
-                pp(response)
-                dlive_user = parse_user(response)
+                json_response = session.post(END_POINT, json=batch).json()
+                dlive_user = parse_user(json_response)
             except Exception as e:
                 print(e)
                 if retries == 0:
-                    return False
+                    break
                 sleep(5)
                 continue
             break
+
         if not dlive_user:
-            dlive_users.update({user: {}})
             continue
+
         dlive_users.update(dlive_user)
     return dlive_users
 
 
-pp(dlive_request(['Grimoire', 'hawkshredder42', 'JuAsJaEn']))
+
+if __name__ == "__main__":
+    from pprint import pprint as pp
+
+    pp( dlive_request('hawkshredder42') )
+    pp( dlive_request('https://dlive.tv/hawkshredder42') )
+    pp( dlive_request(['https://dlive.tv/hawkshredder42', 'JuAsJaEn', 'ericwilson', 'MG_Live', 'RCG_WESKER', 'JiamitdemWesen', 'Millenay', 'RushkaGG', 'eaeNiko', 'MCJams', 'twiggy5000', 'Miroloji', 'Luthien', 'katzeMLBB', 'youlost']) )
